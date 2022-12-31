907feab023987bd082a7e5419d449cb18a866d66
Sumitjh26997 <sumit.hotchandani@gmail.com>
2022-12-31 15:48:35 +0100

    add sha256 code and tests

---
+++ cryptography/hashing/sha256.js
@@ -0,0 +1,19 @@
+const { sha256 } = require("ethereum-cryptography/sha256");
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
+
+// the possible colors that the hash could represent
+const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
+
+// given a hash, return the color that created the hash
+function findColor(hash) {
+    let colorMatch;
+    COLORS.forEach((element) => {
+        if(toHex(hash) === toHex(sha256(utf8ToBytes(element))))
+            colorMatch = element;
+            return;
+    });
+
+    return colorMatch;
+}
+
+module.exports = findColor;

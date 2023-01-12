f792f67e7bd5f6f5e82f6b22f973bb5337811a9f
Sumitjh26997 <sumit.hotchandani@gmail.com>
2023-01-12 14:50:28 +0100

    add mining script sim

---
+++ mining/index.js
@@ -0,0 +1,46 @@
+const SHA256 = require('crypto-js/sha256');
+const TARGET_DIFFICULTY = BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+
+const mempool = [];
+const blocks = [];
+
+function block(id) {
+    this.id = id;
+}
+
+function addTransaction(transaction) {
+    mempool.push(transaction);
+}
+
+function mine() {
+    let hashOutput;
+    let nonce = -1;
+
+    newBlock = new block(blocks.length);
+    newBlock.transactions = [];
+    while(newBlock.transactions.length < MAX_TRANSACTIONS && mempool.length != 0) {
+        let i = 0;
+        newBlock.transactions.push(mempool[i]);
+        mempool.splice(0,1);
+        i++;
+    }
+    while(1){
+        newBlock.nonce = 0;
+        newBlock.hash = SHA256(JSON.stringify(newBlock));
+        if(BigInt(`0x${newBlock.hash}`) < TARGET_DIFFICULTY){
+            break;
+        }
+        newBlock.nonce++;
+    } 
+    blocks.push(newBlock);
+}
+
+module.exports = {
+    TARGET_DIFFICULTY,
+    MAX_TRANSACTIONS,
+    addTransaction, 
+    mine, 
+    blocks,
+    mempool
+};

396668f710e3d6380c8a885c2078536ae54c2828
Sumitjh26997 <sumit.hotchandani@gmail.com>
2023-05-09 15:34:51 +0200

    add test cases

--- blockchain_structure/Blockchain.js
+++ blockchain_structure/Blockchain.js
@@ -9,6 +9,18 @@ class Blockchain {
         block.previousHash = this.chain[this.chain.length - 1].toHash();
         this.chain.push(block);
     }
+
+    isValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+            const valid = currentBlock.previousHash.toString() === previousBlock.toHash().toString();
+            if(!valid) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 module.exports = Blockchain;

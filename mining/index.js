7ca7fdf2f506e16c2c2236727c5035e15d18c685
Sumitjh26997 <sumit.hotchandani@gmail.com>
2023-05-09 13:40:01 +0200

    update mining script

--- mining/index.js
+++ mining/index.js
@@ -14,25 +14,27 @@ function addTransaction(transaction) {
 }
 
 function mine() {
-    let hashOutput;
-    let nonce = -1;
-
-    newBlock = new block(blocks.length);
+    // new block created with id set to the height of the blocks array
+    const newBlock = new block(blocks.length);
     newBlock.transactions = [];
+    
     while(newBlock.transactions.length < MAX_TRANSACTIONS && mempool.length != 0) {
         let i = 0;
         newBlock.transactions.push(mempool[i]);
         mempool.splice(0,1);
         i++;
     }
+
+    newBlock.nonce = 0;
+    
     while(1){
-        newBlock.nonce = 0;
         newBlock.hash = SHA256(JSON.stringify(newBlock));
         if(BigInt(`0x${newBlock.hash}`) < TARGET_DIFFICULTY){
             break;
         }
         newBlock.nonce++;
     } 
+
     blocks.push(newBlock);
 }
 

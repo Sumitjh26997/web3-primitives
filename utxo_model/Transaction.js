aeeea91d0f532ddf77aa3b451e0af692084c69fa
Sumitjh26997 <sumit.hotchandani@gmail.com>
2023-05-25 11:15:27 +0200

    added check for insufficent balance

--- utxo_model/Transaction.js
+++ utxo_model/Transaction.js
@@ -9,6 +9,18 @@ class Transaction {
 							throw new Error("UTXO has been already spent");
 					}
 			});
+
+			const inputValue = this.inputUTXOs.reduce((total, current) => {
+				return total += current.amount;
+			}, 0);
+
+			const outputValue = this.outputUTXOs.reduce((total, current) => {
+					return total += current.amount;
+			}, 0);
+
+			if(inputValue < outputValue) {
+					throw new Error("Insufficient Balance");
+			}
 	}
 }
 
